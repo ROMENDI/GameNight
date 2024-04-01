4ba67328353654e9650b8d36e90216465bca2f9f
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-04-01 20:40:09 +0000

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'games/index'
+  get 'games/show'
+  get 'games/new'
+  get 'games/edit'
+  get 'games/create'
+  get 'games/update'
+  get 'games/destroy'
+  get 'reservations/create'
+  get 'reservations/update'
+  get 'reservations/destroy'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'events/index' 
+  get 'events/show'
+  get 'events/new'
+  get 'events/edit'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
