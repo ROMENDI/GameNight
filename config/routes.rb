8e67fa845831f40b0c52b414001b391ea915844b
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-04-02 19:48:12 +0000

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,18 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'games/index'
-  get 'games/show'
-  get 'games/new'
-  get 'games/edit'
-  get 'games/create'
-  get 'games/update'
-  get 'games/destroy'
-  get 'reservations/create'
-  get 'reservations/update'
-  get 'reservations/destroy'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'events/index' 
-  get 'events/show'
-  get 'events/new'
-  get 'events/edit'
+  # Devise routes for authentication
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
+  # Root route
   root "home#index"
+
+  resources :games
+
+  resources :events
+
+  resources :events do
+    resources :reservations, only: [:create, :update, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
+
 end
