0c45b820446cfa86643726709c62954fc955c279
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-04-03 20:35:21 +0000

    updated ui

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,30 @@
 class ReservationsController < ApplicationController
   def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.sender = current_user
+
+    if @reservation.save
+      redirect_to event_path(@reservation.event), notice: "Reservation successfully created."
+    else
+      render "events/show", status: :unprocessable_entity
+    end
   end
 
   def update
+    @reservation = current_user.reservations.find(params[:id])
+
+    if @reservation.update(reservation_params)
+      redirect_to event_path(@reservation.event), notice: "Reservation successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @reservation = current_user.reservations.find(params[:id])
+    event = @reservation.event
+    @reservation.destroy
+    redirect_to event_path(event), notice: 'Reservation successfully canceled.'
   end
+
 end
