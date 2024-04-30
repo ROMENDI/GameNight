57df2457fa2b8f8be6837cff70a191c5af342e90
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-04-30 21:55:27 +0000

    typo fix

--- db/migrate/20240327203339_create_reservations.rb
+++ db/migrate/20240327203339_create_reservations.rb
@@ -1,6 +1,6 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
-    create_table :reservations do |t|r
+    create_table :reservations do |t|
       t.references :sender, null: false, foreign_key: {to_table: :users}
       t.references :event, null: false, foreign_key: true
       t.integer :status
